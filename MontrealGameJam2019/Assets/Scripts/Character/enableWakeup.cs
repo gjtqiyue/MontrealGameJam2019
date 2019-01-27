924272c256e8114419574280eb1fc0f6e1a1a6fe
Zeynab <znb.mir@gmail.com>
2019-01-26 23:07:39 -0500

    intro animation

---
+++ MontrealGameJam2019/Assets/Scripts/Character/enableWakeup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enableWakeup : MonoBehaviour
+{
+  public Animator coffinDoorAnim;
+  public Animator malcolmAnim;
+  public Animator characterAnim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (coffinDoorAnim.GetCurrentAnimatorStateInfo(0).IsName("RotateDoor"))
+      characterAnim.SetTrigger("EnableWakeup");
+
+    }
+}
