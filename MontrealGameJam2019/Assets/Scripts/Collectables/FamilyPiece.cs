6da6b783db6242c5538f79cb32488eb7c622cfc1
Mat <FVr>
2019-01-27 11:23:30 -0500

    sound

--- MontrealGameJam2019/Assets/Scripts/Collectables/FamilyPiece.cs
+++ MontrealGameJam2019/Assets/Scripts/Collectables/FamilyPiece.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class FamilyPiece : Collectable
 {
+    public AK.Wwise.Event Memory;
     public int pieceNum;
-
     public override void GetCollected(CharacterScript player)
     {
         // call the method to add memory
         Debug.Log("Get memory");
         if (player != null) player.ReceiveMemory(pieceNum);
-    }
+        Memory.Post(gameObject);
+             
+             }
 }
+
